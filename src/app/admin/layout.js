ab3fb9a91d331844fd9cf8a070eabbe0bfc979b1
AnujChaudhari <kc812213@gmail.com>
2026-05-17 12:38:09 +0530

    Delete src/app/admin directory

--- src/app/admin/layout.js
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-import AdminClientLayout from './client-layout';
-
-export default function AdminLayout({ children }) {
-  return <AdminClientLayout>{children}</AdminClientLayout>;
-}
