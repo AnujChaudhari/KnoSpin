833e76eb3be839740391fb2d02fdd2f5726c530c
AnujChaudhari <kc812213@gmail.com>
2026-05-17 18:06:03 +0530

    Create layout.js

---
+++ src/app/admin/layout.js
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import AdminClientLayout from './client-layout';
+
+export default function AdminLayout({ children }) {
+  return <AdminClientLayout>{children}</AdminClientLayout>;
+}
