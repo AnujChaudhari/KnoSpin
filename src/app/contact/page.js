12b9797b1e1d5fef8107d70debeea7db62582185
AnujChaudhari <kc812213@gmail.com>
2026-05-18 19:28:19 +0530

    Create page.js

---
+++ src/app/contact/page.js
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic';
+
+export default function ContactPage() {
+  return (
+    <div className="max-w-2xl mx-auto p-4 my-8">
+      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      <p>Contact information will be here thinklabvies@gmail.com or +91 74411 67537 .</p>
+    </div>
+  );
+}
